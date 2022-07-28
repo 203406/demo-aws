df886ec19a733c3cccefe2228dbcbab274e9385b
203406 <203406@ids.upchiapas.edu.mx>
2022-07-28 13:25:17 -0500

    all

--- src/main/java/com/demo/demoaws/Controller/UserController.java
+++ src/main/java/com/demo/demoaws/Controller/UserController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("name")
     public String home(){
-        return "Hi, my name is daniel";
+        return "Hi, my name is daniell";
     }
 }
